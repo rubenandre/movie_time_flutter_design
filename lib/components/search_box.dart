e0268c4064ffe33c13bc81c6ccb286b59fd2d74b
Rúben Silva <rubensilva945@gmail.com>
2020-01-26 02:51:04 +0000

    feat(searchbox): added real input text field

--- lib/components/search_box.dart
+++ lib/components/search_box.dart
@@ -7,10 +7,23 @@ Widget SearchBox() {
     child: Padding(
       padding: EdgeInsets.only(left: 10),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Icon(Icons.search, color: Color(0xFFb39ed4), size: 30,), 
           SizedBox(width: 15),
-          Text('Search', style: TextStyle(color: Color(0xFFb39ed4), fontSize: 22),)
+          Container(
+            child: TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Search',
+                hintStyle: TextStyle(color: Color(0xFFb39ed4), fontSize: 20),
+              ),
+              cursorColor: Color(0xFFb39ed4),
+              style: TextStyle(color: Color(0xFFb39ed4), fontSize: 20),
+            ), 
+            height: double.infinity, 
+            width: 200,
+          )
         ],
       ),
     ),
